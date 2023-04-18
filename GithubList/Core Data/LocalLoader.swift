dcb2a33e883c5c000067e43be46bb22cbaf2cfe1
Wan Lutfi <lutfime_2000@yahoo.com>
2023-04-18 11:01:23 +0800

    Added `LocalLoader`, a loader to load core data that implements `UsersLoaderProtocol`

--- GithubList/Core Data/LocalLoader.swift
+++ GithubList/Core Data/LocalLoader.swift
@@ -15,7 +15,27 @@ class LocalLoader: UsersLoader{
     }
     
     func loadGithubUsers(startUserIndex: Int, completion: @escaping (Result<[User], Error>) -> Void) {
-        
+        let users = dataProvider.getUsers()?.map({$0.toModel()})
+        completion(.success(users ?? []))
     }
     
+}
+
+extension UserManagedObject{
+    func toModel() -> User{
+        let user = User()
+        user.id = Int(userId)
+        user.loginName = loginName
+        user.avatarURL = avatarURL
+        user.profileURL = profileURL
+        user.starredURL = starredURL
+        user.name = name
+        user.company = company
+        user.blog = blog
+        user.location = location
+        user.email = email
+        user.followerCount = Int(followerCount)
+        user.followingCount = Int(followingCount)
+        return user
+    }
 }
