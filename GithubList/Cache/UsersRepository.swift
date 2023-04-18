1f36334106426a79f9bd67fa0194af78b998dd2b
Wan Lutfi <lutfime_2000@yahoo.com>
2023-04-19 07:20:43 +0800

    Added `UsersRepository` as infra adapter for `UsersCoreDataRepository`

---
+++ GithubList/Cache/UsersRepository.swift
@@ -0,0 +1,14 @@
+//
+//  UsersRepository.swift
+//  GithubList
+//
+//  Created by Wan Ahmad Lutfi on 19/04/2023.
+//
+
+import Foundation
+
+protocol UsersRepository{
+    func getUsers() -> [User]?
+    func getUser(with loginName: String) -> User
+    func save(_ users: [User])
+}
