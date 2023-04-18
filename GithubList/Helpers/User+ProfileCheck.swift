46a579c89862fbd73fd9f0f77a08569e83b02f39
Wan Lutfi <lutfime_2000@yahoo.com>
2023-04-18 13:51:50 +0800

    Rename `Utils` folder to `Helpers`

---
+++ GithubList/Helpers/User+ProfileCheck.swift
@@ -0,0 +1,18 @@
+//
+//  User+ProfileCheck.swift
+//  GithubList
+//
+//  Created by Wan Ahmad Lutfi on 18/04/2023.
+//
+
+import Foundation
+
+extension User{
+    ///Check if profile data already available in the current user
+    func isProfileAvailable() -> Bool{
+        if name != nil || email != nil || notes != nil{
+            return true
+        }
+        return false
+    }
+}
